25e7d6a5359db0da0220c894f70e448babdf6369
Rafael Pimenta <rafaelpimentadm@gmail.com>
2025-07-09 22:57:52 -0300

    adjust message errors

--- api/handler.py
+++ api/handler.py
@@ -18,25 +18,32 @@ app = Flask(__name__)
 
 @app.route('/HotelSmart/predict', methods=['POST'])
 def cancellation_predict():
-    test_json = request.get_json()
+    try:
+        test_json = request.get_json()
 
-    if test_json:
-        if isinstance(test_json, dict):
-            test_raw = pd.DataFrame(test_json, index=[0])
+        if test_json:
+            if isinstance(test_json, dict):
+                test_raw = pd.DataFrame(test_json, index=[0])
+            else:
+                test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+            pipeline = PredictCancellation()
+            df_cleaning = pipeline.convert_binary_columns(test_raw)
+            df_feature = pipeline.feature_engineering(df_cleaning)
+            df_preparation = pipeline.data_preparation(df_feature)
+
+            df_predict = pipeline.get_predictions(model, df_preparation, test_raw)
+
+            return Response(df_predict, status=200, mimetype='application/json')
         else:
-            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+            return Response('{}', status=200, mimetype='application/json')
 
-        pipeline = PredictCancellation()
-        df_cleaning = pipeline.convert_binary_columns(test_raw)
-        df_feature = pipeline.feature_engineering(df_cleaning)
-        df_preparation = pipeline.data_preparation(df_feature)
+    except Exception as e:
+        print(f"❌ ERRO NA API: {e}")
+        return Response(f'{{"error": "{str(e)}"}}', status=500, mimetype='application/json')
 
-        df_predict = pipeline.get_predictions(model, df_preparation, test_raw)
 
-        return Response(df_predict, status=200, mimetype='application/json')
-    else:
-        return Response('{}', status=200, mimetype='application/json')
-
+# Executa localmente apenas se chamado diretamente
 if __name__ == "__main__":
-    port = os.environ.get('PORT', 5000)
-    app.run('0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
