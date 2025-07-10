7db2174e581022dfe4fd9b0758c34268edf5f0fd
Rafael Pimenta <rafaelpimentadm@gmail.com>
2025-07-10 00:53:15 -0300

    coming back to old handler

--- api/handler.py
+++ api/handler.py
@@ -18,33 +18,25 @@ app = Flask(__name__)
 
 @app.route('/HotelSmart/predict', methods=['POST'])
 def cancellation_predict():
-    try:
-        test_json = request.get_json()
+    test_json = request.get_json()
 
-        if test_json:
-            if isinstance(test_json, dict):
-                test_raw = pd.DataFrame(test_json, index=[0])
-            else:
-                test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
-
-            pipeline = PredictCancellation()
-            df_cleaning = pipeline.convert_binary_columns(test_raw)
-            df_feature = pipeline.feature_engineering(df_cleaning)
-            df_preparation = pipeline.data_preparation(df_feature)
-
-            df_predict = pipeline.get_predictions(model, df_preparation, test_raw)
-
-            return Response(df_predict, status=200, mimetype='application/json')
+    if test_json:
+        if isinstance(test_json, dict):
+            test_raw = pd.DataFrame(test_json, index=[0])
         else:
-            return Response('{}', status=200, mimetype='application/json')
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
 
-    except Exception as e:
-        print("❌ ERRO NA API:")
-        traceback.print_exc()  # ✅ Mostra o erro completo no log do Koyeb
-        return Response(f'{{"error": "{str(e)}"}}', status=500, mimetype='application/json')
+        pipeline = PredictCancellation()
+        df_cleaning = pipeline.convert_binary_columns(test_raw)
+        df_feature = pipeline.feature_engineering(df_cleaning)
+        df_preparation = pipeline.data_preparation(df_feature)
 
+        df_predict = pipeline.get_predictions(model, df_preparation, test_raw)
 
-# Executa localmente apenas se chamado diretamente
+        return Response(df_predict, status=200, mimetype='application/json')
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=port)
